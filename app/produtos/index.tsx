e88402a04b4a4ff6a41f400f759232d647c8e484
hDepth <dephenriquejp@gmail.com>
2025-05-16 14:04:08 -0300

    Estruturação e config Expo-router

---
+++ app/produtos/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function Produtos() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 20 }}>Lista de Produtos</Text>
+    </View>
+  );
+}
